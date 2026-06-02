a62533c954aaf3114ada91f0e1ca9130e151aa53
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-06-02 13:29:58 +0500

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,18 @@
+
+######## While loop practice ###########
+
+
+# a = 10
+# b = 5
+# while a > b:
+#     print("you are right")
+
+
+# Example 2
+a = 10
+b = 5
+
+while a > b:
+    print("yes a is bigger then b")
+    b = b + 1
+print("while loop ended")
