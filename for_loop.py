e531e2a8927777b9ddf4dc2bb3e0841cc68c0369
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-06-03 13:40:29 +0500

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,26 @@
+thislist = ["apple", "banana", "cherry","apple_2", "banana_2", "cherry_1","apple_3", "banana_3", "cherry_2","apple_4", "banana_4", "cherry_4","apple_5", "banana_5", "cherry_5"]
+print(thislist[0])
+print(thislist[1])
+print(thislist[2])
+print(thislist[3])
+print(thislist[4])
+print(thislist[5])
+print(thislist[6])
+print(thislist[7])
+print(thislist[8])
+print(thislist[9])
+print(thislist[10])
+
+print("######################################################")
+print("#############  Below out put is via for loop #########")
+
+#################
+# Syntax of for loop
+###################
+# for [veriable] in [list, string, touple, dictionary, etec]:
+    # print("This is syntaxt")
+
+###############################
+for x in thislist:
+    print(x)
+
