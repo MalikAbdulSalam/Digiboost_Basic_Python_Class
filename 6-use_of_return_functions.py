af1cb92e7487d985abe84d11016f270ad8f1b2d3
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-06-10 17:23:53 +0500

    Add files via upload

---
+++ 6-use_of_return_functions.py
@@ -0,0 +1,34 @@
+# # Use of Return in functions
+#
+# def my_first_Function():
+#     print("Bismillah")
+#     return "this is flag"
+#
+# veribl = my_first_Function()
+# print(veribl)
+#
+# print(my_first_Function())
+#
+# print("###########################################")
+#
+# def saad():
+#     print("saad")
+#     pass
+# saad()
+#
+#
+#
+# print("###########################################")
+# def my_function(x):
+#     print("this is string", x)
+#
+# my_function("hello")
+# my_function(1)
+#
+# print("##########################################")
+# Default Parameters
+def default_parameters(name, intstitute_name = "DBI"):
+  print("Hello", name, intstitute_name)
+
+
+default_parameters("saad")
