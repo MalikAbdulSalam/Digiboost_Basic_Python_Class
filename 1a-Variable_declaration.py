d0dae0006edb37472df46b47da59f63f7f7a4744
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-06-21 12:37:55 +0500

    Variable understanding

---
+++ 1a-Variable_declaration.py
@@ -0,0 +1,22 @@
+Huzaifa = 100000000
+y = 400
+a = "this is school"
+b = 10.252
+
+
+print(Huzaifa)
+
+print("Huzaifa")
+
+
+print(y)
+
+print("###########################################")
+
+print(a)
+
+print("###########################################")
+
+print(b)
+
+# changing values in variables
---
+++ 1a-Variable_vaule_changing.py
@@ -0,0 +1,9 @@
+variable_1 = 2
+
+print(variable_1)
+
+print("##################################")
+
+variable_1 = 2000000
+
+print(variable_1)
